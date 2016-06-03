a137136880b1222e98406a9fbe625ae4e80688e7
ping-liang <ping.liang08@gmail.com>
2016-06-03 11:36:17 -0400

    Unity references to UnityApp

---
+++ UnityApps/UnityApps/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
